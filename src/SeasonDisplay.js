0d01d4fef7db6aa6f8ceb0f9bc9070d17ed4a32c
Zach Ioannou <zioannou77@gmail.com>
2020-01-02 20:50:56 +0200

    added logic for calculating seasons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "Summer" : "Winter";
+  } else {
+    return lat > 0 ? "Winter" : "Summer";
+  }
+};
+
 const SeasonDisplay = props => {
-  return <div>Latitude: {props.lat}</div>;
+  return getSeason(props.lat, new Date().getMonth());
 };
 
 export default SeasonDisplay;
