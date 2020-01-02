49464026bad6e876284af85af2d915542ac01175
Zach Ioannou <zioannou77@gmail.com>
2020-01-02 20:35:48 +0200

    refactored constructor state initialization

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // THIS IS THE ONLY TIME we do direct assignment
-    // to this.state
-    this.state = { lat: null, errorMessage: "" };
-  }
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
