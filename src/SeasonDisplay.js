d94b39aac139d2b2ecdf2afeb9515eb655f1b433
Zach Ioannou <zioannou77@gmail.com>
2020-01-03 18:50:15 +0200

    refactoring SeasonDisplay component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,23 +1,33 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "It is warm let's go surfing",
+    iconName: "Sun"
+  },
+  winter: {
+    text: "It is cold let's go snow boarding",
+    iconName: "snowflake"
+  }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? "Summer" : "Winter";
+    return lat > 0 ? "summer" : "winter";
   } else {
-    return lat > 0 ? "Winter" : "Summer";
+    return lat > 0 ? "winter" : "summer";
   }
 };
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const message =
-    season === "Winter"
-      ? "It is cold lets go snow boarding"
-      : "It is warm lets go surfing";
+  const { text, iconName } = seasonConfig[season];
 
   return (
     <div>
-      <h1>{message}</h1>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
