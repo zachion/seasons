c13daedfc838228c48950f707a7c5d27fb4f5f68
Zach Ioannou <zioannou77@gmail.com>
2020-01-04 13:33:11 +0200

    styling spinner

--- src/Spinner.js
+++ src/Spinner.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const Spinner = props => {
   return (
-    <div className="ui active dimmer">
+    <div className="ui active dimmer border darkred">
       <div className="ui big text loader">{props.message}</div>
     </div>
   );
+};
+
+Spinner.defaultProps = {
+  message: 'Loading...'
 };
 
 export default Spinner;
