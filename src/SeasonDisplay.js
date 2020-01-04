1df85abfb3cfc32eddedb5ae126d55fae78a2f1b
Zach Ioannou <zioannou77@gmail.com>
2020-01-04 12:19:34 +0200

    added SeasonDisplay style template

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,22 +1,22 @@
-import React from "react";
+import './SeasonDisplay.css';
+import React from 'react';
 
 const seasonConfig = {
   summer: {
     text: "It is warm let's go surfing",
-    iconName: "Sun"
+    iconName: 'Sun'
   },
   winter: {
     text: "It is cold let's go snow boarding",
-    iconName: "snowflake"
+    iconName: 'snowflake'
   }
 };
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? "summer" : "winter";
-  } else {
-    return lat > 0 ? "winter" : "summer";
+    return lat > 0 ? 'summer' : 'winter';
   }
+  return lat > 0 ? 'winter' : 'summer';
 };
 
 const SeasonDisplay = props => {
@@ -24,10 +24,10 @@ const SeasonDisplay = props => {
   const { text, iconName } = seasonConfig[season];
 
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`massive icon-left ${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`massive icon-right ${iconName} icon`} />
     </div>
   );
 };
